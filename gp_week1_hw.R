48a6a94c5e92422e2675ee93cb9d11397df3cc95
Greg Parker <parkeg08@uwosh.edu>
2016-09-04 14:17:29 -0700

    Added temp .docx file to ignore

--- gp_week1_hw.R
+++ gp_week1_hw.R
@@ -1,4 +1,19 @@
-# Author:     Greg Parker
-# Date:       04 Sep 2016
-#assignment:  Week 1 HW
+# Author:       Greg Parker
+# Date:         04 Sep 2016
+# Assignment:   Week 1 HW
 
+# Task 1.0: Open R.  The first line of text in the console 
+#   window tells you which version of R you are running 
+#   (this should be version 3.1.2).  Copy this line of text
+#   into a document, to verify that you’ve installed the 
+#   correct version.
+# R version 3.3.1 (2016-06-21) -- "Bug in Your Hair"
+
+# Task 1.1:  Calculate the cube root of 2015, as follows:
+2015^(1/3)
+# Output:  [1] 12.63063
+
+# Task 1.2:  Find the absolute value of 5.7 minus 6.8 
+#   divided by .58:
+abs(5.7-6.8)/.58
+# Output:  [1] 1.896552
